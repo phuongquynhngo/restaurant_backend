c49925c286cc810ac45e538141ef49777f99de16
Quynh Ngo <ngophuongquynh25@gmail.com>
2023-08-24 12:39:15 +0200

    modify environment variables

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const pool = mysql.createPool({
-   host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'restaurant'
+   host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 }).promise()
 
 // const result = await pool.query("SELECT * FROM customers")
