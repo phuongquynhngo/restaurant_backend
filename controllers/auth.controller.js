0c871acd0ebd4025028036bd12f67ab71423ce8b
Quynh Ngo <ngophuongquynh25@gmail.com>
2023-09-05 17:21:11 +0200

    modify auth controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -22,53 +22,61 @@ dotenv.config();
 // }
 
 const handleLogin = async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      // Find the user in the database
-      const foundUser = await User.findOne({ where: { username } });
-  
-      if (!foundUser) {
-        return res.status(401).json({ message: 'Unauthorized' });
-      }
-  
-      // Verify the password
-      const isPasswordValid = await bcrypt.compare(password, foundUser.password);
-  
-      if (isPasswordValid) {
-        // Create JWTs
-        const accessToken = jwt.sign(
-          { username: foundUser.username },
-          process.env.ACCESS_TOKEN_SECRET,
-          { expiresIn: '30s' }
-        );
-        const refreshToken = jwt.sign(
-          { username: foundUser.username },
-          process.env.REFRESH_TOKEN_SECRET,
-          { expiresIn: '1d' }
-        );
-  
-        // Update the user's refreshToken in the database (invalidate when user logouts)
-        // foundUser.refreshToken = refreshToken;
-        // await foundUser.save();
-  
-        // Set the refreshToken as an HTTP-only cookie
-        res.cookie('jwt', refreshToken, {
-          httpOnly: true,
-          sameSite: 'None',
-          secure: true,
-          maxAge: 24 * 60 * 60 * 1000,
-        });
-  
-        res.json({ accessToken });
-      } else {
-        res.status(401).json({ message: 'Unauthorized' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
+  const { username, password } = req.body;
+
+  try {
+    // Find the user in the database
+    const foundUser = await User.findOne({ where: { username } });
+
+    if (!foundUser) {
+      return res.status(401).json({ message: 'Unauthorized' });
     }
-  };
-  
+
+    // Verify the password
+    const isPasswordValid = await bcrypt.compare(password, foundUser.password);
+
+    if (isPasswordValid) {
+      // Retrieve user roles
+      const userRoles = await foundUser.getRoles();
+
+      // Construct authorities array
+      const authorities = userRoles.map(role => `${role.name}`);
+
+      // Create JWTs
+      const accessToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '300s' }
+      );
+      const refreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '1d' }
+      );
+
+      // Set the refreshToken as an HTTP-only cookie
+      res.cookie('jwt', refreshToken, {
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+
+      res.json({
+        id: foundUser.id,
+        username: foundUser.username,
+        email: foundUser.email,
+        roles: authorities,
+        accessToken,
+      });
+    } else {
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
 
 export default { handleLogin };
